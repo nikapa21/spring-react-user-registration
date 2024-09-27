069b7520da0d2f55b696531f1f14b8251382940a
nkaragounis <nkaragounis@lgcns.com>
2024-09-28 02:11:53 +0300

    Setup project

---
+++ src/main/java/org/consoleconnect/spring_react_user_registration/SpringReactUserRegistrationApplication.java
@@ -0,0 +1,13 @@
+package org.consoleconnect.spring_react_user_registration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringReactUserRegistrationApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(SpringReactUserRegistrationApplication.class, args);
+    }
+}
---
+++ src/test/java/org/consoleconnect/spring_react_user_registration/SpringReactUserRegistrationApplicationTests.java
@@ -0,0 +1,13 @@
+package org.consoleconnect.spring_react_user_registration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringReactUserRegistrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
